240ec124da07a3e975085c30a457c5e1dfaa0d77
Yavuz Selim Gügen <ygugen@windowslive.com>
2020-05-10 02:26:09 +0300

    Course CRUD and interface added

---
+++ networkHW/Models/OBSContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace networkHW.Models
+{
+    public class OBSContext : DbContext
+    {
+        public OBSContext(DbContextOptions<OBSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ networkHW/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace networkHW.Models
+{
+    public class Student
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int No { get; set; }
+        public Course Courses { get; set; }
+    }
+}
